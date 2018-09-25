77bfc1b9f1e7970db88e616ede9b45bb2d263cff
DESKTOP-P7R113A\Alex <alejandromartinezpalanques@gmail.com>
2018-09-25 18:35:28 +0200

    Initial commit

---
+++ app/src/main/java/prueba/amp/palanques/myapplication/PalanquesActivity.java
@@ -0,0 +1,13 @@
+package prueba.amp.palanques.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PalanquesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palanques);
+    }
+}
